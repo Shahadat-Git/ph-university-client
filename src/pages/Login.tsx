0c20f43e01fd0d670ac63531723e4165a0c798e0
Shahadat-Git <noyon560@gmail.com>
2024-01-19 01:45:09 +0600

    set token and user data from response to local state using jwt decode

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,17 +1,33 @@
 import { Button } from "antd";
 import { useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useAppDispatch } from "../redux/hooks";
+import { setUser } from "../redux/features/auth/authSlice";
+import { verifyToken } from "../utils/verifyToken";
 
 const Login = () => {
-  const { register, handleSubmit } = useForm();
+  const dispatch = useAppDispatch();
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      id: "A-0001",
+      password: "admin123",
+    },
+  });
 
-  const [login, { data, error }] = useLoginMutation();
-  console.log("data =>", data);
+  const [login, { error }] = useLoginMutation();
+  // console.log("data =>", data);
   console.log("error =>", error);
 
-  const onSubmit = (data) => {
-    console.log(data);
-    login(data);
+  const onSubmit = async (data) => {
+    const userInfo = {
+      id: data.id,
+      password: data.password,
+    };
+    const res = await login(userInfo).unwrap();
+    const user = verifyToken(res.data.accessToken);
+
+    dispatch(setUser({ user: user, token: res.data.accessToken }));
+    console.log(res);
   };
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
